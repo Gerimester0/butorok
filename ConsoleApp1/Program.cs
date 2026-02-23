ae81fc2db45676f54fd0d192353401df78771056
PappG <PappG@310-14>
2026-02-23 08:12:55 +0100

    Kivételkezelés

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace ConsoleApp1
+﻿using System.Text.Json;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            try
+            {
+                string file = File.ReadAllText("butorok.json");
+                Gyoker gy = JsonSerializer.Deserialize < Gyoker>(file);
+            }
+            catch (JsonException ex)
+            {
+                Console.WriteLine("JSON fájl feldolgozási hiba:" + ex.Message);
+            }
+
+            catch (FileNotFoundException ex) 
+            {
+                Console.WriteLine("Fájl elérési hiba." + ex.Message);
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Fájlkezelési hiba." + ex.Message);
+            }
+
         }
     }
 }
---
+++ ConsoleApp1/Targyak.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Targyak
+    {
+        public int azonosito { get; set; }
+
+        public string nev { get; set; }
+
+        public string anyag { get; set; }
+
+        public Meretek meretek { get; set; }
+
+        public int ar { get; set; }
+                
+        public bool keszleten { get; set; }
+    }
+}
